df33e33b9305afcb23f7baf6cb84bff4e6b5582f
arendellian13 <listener1381@outlook.com>
2024-02-15 22:27:52 +0800

    Add Base64 Decode and Encode Functor

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <utility>
 
 namespace Krypton {
 
@@ -17,5 +18,25 @@ ByteArray fromBase64(const std::string&);
 std::string toHex(const ByteArray&);
 ByteArray fromHex(const std::string&);
 inline ByteArray toBuffer(const std::string& str) { return static_cast<ByteArray>(str); }
+
+template <typename PrevFunctor>
+class Base64Encode {
+public:
+    template <typename... Args>
+    std::string operator()(const Args&... rest)
+    {
+        return toBase64(PrevFunctor(std::forward<Args>(rest)...));
+    }
+};
+
+template <typename PrevFunctor>
+class Base64Decode {
+public:
+    template <typename... Args>
+    ByteArray operator()(const Args&... rest)
+    {
+        return fromBase64(PrevFunctor(std::forward<Args>(rest)...));
+    }
+};
 
 }

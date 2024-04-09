e5db9699b5ac898a09a27d4ca42a22a595f4035d
arendellian13 <listener1381@outlook.com>
2024-04-09 14:24:15 +0800

    update functor

--- include/CPSBase.hpp
+++ include/CPSBase.hpp
@@ -12,6 +12,9 @@ namespace Detail {
     };
 }
 
+template <typename F, typename... Args>
+using ReturnType = typename std::invoke_result<F(Args..)>::type;
+
 template <int Index>
 struct GetKthArgument {
     template <typename... Args>
@@ -19,6 +22,16 @@ struct GetKthArgument {
     {
         auto t = std::forward_as_tuple(std::forward<Args>(args)...);
         return std::get<Index>(t);
+    }
+};
+
+template <typename PrevFunctor, typename... LambdaPara>
+struct LambdaBinder {
+    template <typename... Args>
+    auto operator()(Args&&... args)
+    {
+        auto f = PrevFunctor {}(std::forward<Args>(args)...);
+        return f(LambdaPara {}(std::forward<Args>(args)...)...);
     }
 };
 
--- tests/CPSBaseUnitTest.cpp
+++ tests/CPSBaseUnitTest.cpp
@@ -19,3 +19,19 @@ TEST(CPSBaseUnitTest, GetParaTest1)
     ASSERT_EQ(Func1 {}(1, 2, 3, 4, 5, 6), 1);
     ASSERT_EQ(Func2 {}(1, 2, 3), 3);
 }
+
+TEST(CPSBaseUnitTest, FunctorTest1)
+{
+    int x = 1;
+    using Func1 = TestFunctor1<LambdaBinder<GetKthArgument<0>>>;
+    auto f = [&]() { return x; };
+    ASSERT_EQ(Func1 {}(f), 1);
+    x = 10;
+    ASSERT_EQ(Func1 {}(f), 10);
+    auto f2 = std::bind(Func1 {}, f);
+    ASSERT_EQ(f2(), x);
+    auto t = [&](int c) { return x + c; };
+    using Func2 = TestFunctor1<LambdaBinder<GetKthArgument<0>, GetKthArgument<1>>>;
+    auto f3 = std::bind(Func2 {}, t, std::placeholders::_1);
+    ASSERT_EQ(f3(4), x + 4);
+}

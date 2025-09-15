0d16d03512cb0824bd40a98aec8980d726aab068
arendellian13 <listener1381@outlook.com>
2025-09-15 20:23:43 +0800

    feat: Enhance as_bytes_view with non-contiguous support and additional tests

--- include/krypton/common/range.hpp
+++ include/krypton/common/range.hpp
@@ -8,6 +8,7 @@
 #include "common.hpp"
 #include <cstddef>
 #include <iterator>
+#include <type_traits>
 
 #ifdef KRYPTON_USE_STD_RANGES
 #include <ranges>
@@ -22,7 +23,6 @@
 #include <range/v3/view/interface.hpp>
 #include <range/v3/view/view.hpp>
 #endif
-#include <type_traits>
 
 namespace krypton {
 
@@ -50,8 +50,225 @@ using namespace ::ranges::views;
 
 namespace ext {
 
+namespace detail {
+template <typename BaseIter> class contiguous_as_byte_iterator {
+  // Currently the implementation of contiguous_as_byte_iterator is almost the same as the
+  // noncontiguous version. We keep them separate for clarity and potential future optimizations.
+private:
+  BaseIter current_;
+  std::size_t index_ = 0;
+
+public:
+  // It should be a random access iterator, but now we do not implement the operator[], so we
+  // downgrade it to bidirectional.
+  using iterator_category = std::bidirectional_iterator_tag;
+  using value_type        = byte;
+  using difference_type   = std::ptrdiff_t;
+  using pointer           = void; // The proxy type does not support operator->.
+  using reference         = byte; // The proxy type returns by value.
+
+  contiguous_as_byte_iterator() = default;
+
+  explicit contiguous_as_byte_iterator(BaseIter current, std::size_t index = 0)
+      : current_(current), index_(index) {}
+
+  reference operator*() const {
+    auto ptr = reinterpret_cast<const byte *>(std::to_address(current_));
+    return ptr[index_];
+  }
+
+  contiguous_as_byte_iterator &operator++() {
+    ++index_;
+    if (index_ == sizeof(*current_)) {
+      index_ = 0;
+      ++current_;
+    }
+    return *this;
+  }
+
+  contiguous_as_byte_iterator operator++(int) {
+    auto tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+
+  contiguous_as_byte_iterator &operator--() {
+    if (index_ == 0) {
+      --current_;
+      index_ = sizeof(*current_);
+    }
+    --index_;
+    return *this;
+  }
+
+  contiguous_as_byte_iterator operator--(int) {
+    auto tmp = *this;
+    --(*this);
+    return tmp;
+  }
+
+  contiguous_as_byte_iterator &operator+=(difference_type diff) {
+    // The number of bytes in one element of the underlying range.
+    const auto element_size = sizeof(*current_);
+
+    // This can be negative, so use a signed type.
+    const difference_type total_offset = index_ + diff;
+
+    // How many full elements we need to advance (or retreat).
+    const auto element_offset = total_offset / element_size;
+
+    // The new byte index within the target element.
+    // The modulo operator in C++ can yield a negative result for a negative input.
+    // We adjust it to ensure the index is always a valid, non-negative offset.
+    index_ = total_offset % element_size;
+    if (index_ < 0) {
+      index_ += element_size;
+      // We borrowed from the previous element, so adjust the element offset.
+      current_ += element_offset - 1;
+    } else {
+      current_ += element_offset;
+    }
+
+    return *this;
+  }
+
+  contiguous_as_byte_iterator &operator-=(difference_type diff) { return *this += -diff; }
+
+  friend contiguous_as_byte_iterator operator+(contiguous_as_byte_iterator iter,
+                                               difference_type n) {
+    return iter += n;
+  }
+  friend contiguous_as_byte_iterator operator+(difference_type n,
+                                               contiguous_as_byte_iterator iter) {
+    return iter += n;
+  }
+  friend contiguous_as_byte_iterator operator-(contiguous_as_byte_iterator iter,
+                                               difference_type n) {
+    return iter -= n;
+  }
+
+  friend difference_type operator-(contiguous_as_byte_iterator lhs,
+                                   contiguous_as_byte_iterator rhs) {
+    const auto elem_diff = lhs.current_ - rhs.current_;
+    const auto byte_diff = lhs.index_ - rhs.index_;
+    return elem_diff * sizeof(*current_) + byte_diff;
+  }
+
+  friend bool operator==(contiguous_as_byte_iterator lhs,
+                         contiguous_as_byte_iterator rhs) = default;
+
+  friend auto operator<=>(contiguous_as_byte_iterator lhs, contiguous_as_byte_iterator rhs) {
+    return lhs.current_ == rhs.current_ ? lhs.index_ <=> rhs.index_ : lhs.current_ <=> rhs.current_;
+  }
+};
+
+template <typename BaseIter> class noncontiguous_as_byte_iterator {
+  static_assert(ranges::input_iterator<BaseIter>,
+                "The base iterator must satisfy the input iterator concept");
+  static_assert(std::is_copy_assignable_v<typename std::iterator_traits<BaseIter>::value_type>,
+                "The value type of the base iterator must be copy assignable");
+
+private:
+  BaseIter current_;
+  std::size_t index_ = 0;
+
+public:
+  using iterator_category = std::bidirectional_iterator_tag;
+  using value_type        = byte;
+  using difference_type   = std::ptrdiff_t;
+  using pointer           = void; // The proxy type does not support operator->.
+  using reference         = byte; // The proxy type returns by value.
+
+  noncontiguous_as_byte_iterator() = default;
+
+  explicit noncontiguous_as_byte_iterator(BaseIter current, std::size_t index = 0)
+      : current_(current), index_(index) {}
+
+  reference operator*() const {
+    auto res = reinterpret_cast<const byte *>(&(*current_))[index_];
+    return res;
+  }
+
+  noncontiguous_as_byte_iterator &operator++() {
+    ++index_;
+    if (index_ == sizeof(*current_)) {
+      index_ = 0;
+      ++current_;
+    }
+    return *this;
+  }
+
+  noncontiguous_as_byte_iterator operator++(int) {
+    auto tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+
+  noncontiguous_as_byte_iterator &operator--() {
+    if (index_ == 0) {
+      --current_;
+      index_ = sizeof(*current_);
+    }
+    --index_;
+    return *this;
+  }
+
+  noncontiguous_as_byte_iterator operator--(int) {
+    auto tmp = *this;
+    --(*this);
+    return tmp;
+  }
+
+  noncontiguous_as_byte_iterator &operator+=(difference_type diff) {
+    const auto element_size            = sizeof(*current_);
+    const difference_type total_offset = index_ + diff;
+    const auto element_offset          = total_offset / element_size;
+
+    index_ = total_offset % element_size;
+    if (index_ < 0) {
+      index_ += element_size;
+      current_ += element_offset - 1;
+    } else {
+      current_ += element_offset;
+    }
+
+    return *this;
+  }
+
+  noncontiguous_as_byte_iterator &operator-=(difference_type diff) { return *this += -diff; }
+
+  friend noncontiguous_as_byte_iterator operator+(noncontiguous_as_byte_iterator iter,
+                                                  difference_type n) {
+    return iter += n;
+  }
+  friend noncontiguous_as_byte_iterator operator+(difference_type n,
+                                                  noncontiguous_as_byte_iterator iter) {
+    return iter += n;
+  }
+  friend noncontiguous_as_byte_iterator operator-(noncontiguous_as_byte_iterator iter,
+                                                  difference_type n) {
+    return iter -= n;
+  }
+
+  friend difference_type operator-(noncontiguous_as_byte_iterator lhs,
+                                   noncontiguous_as_byte_iterator rhs) {
+    const auto elem_diff = lhs.current_ - rhs.current_;
+    const auto byte_diff = lhs.index_ - rhs.index_;
+    return elem_diff * sizeof(*current_) + byte_diff;
+  }
+
+  friend bool operator==(noncontiguous_as_byte_iterator lhs,
+                         noncontiguous_as_byte_iterator rhs) = default;
+
+  friend auto operator<=>(noncontiguous_as_byte_iterator lhs, noncontiguous_as_byte_iterator rhs) {
+    return lhs.current_ == rhs.current_ ? lhs.index_ <=> rhs.index_ : lhs.current_ <=> rhs.current_;
+  }
+};
+
+} // namespace detail
+
 template <ranges::input_range Rng>
-  requires ranges::contiguous_range<Rng>
+// requires ranges::contiguous_range<Rng>
 class as_bytes_view : public ranges::view_interface<as_bytes_view<Rng>> {
   static_assert(std::is_trivially_copyable_v<ranges::range_value_t<Rng>>,
                 "as_bytes_view requires a range of trivially copyable types");
@@ -61,101 +278,9 @@ private:
   using BaseIter = ranges::iterator_t<Rng>;
 
 public:
-  class iterator {
-  private:
-    BaseIter current_;
-    std::size_t index_ = 0;
-
-  public:
-    using iterator_category = std::random_access_iterator_tag;
-    using value_type        = byte;
-    using difference_type   = std::ptrdiff_t;
-    using pointer           = void; // The proxy type does not support operator->.
-    using reference         = byte; // The proxy type returns by value.
-
-    iterator() = default;
-
-    explicit iterator(BaseIter current, std::size_t index = 0) : current_(current), index_(index) {}
-
-    reference operator*() const {
-      auto ptr = reinterpret_cast<const byte *>(std::to_address(current_));
-      return ptr[index_];
-    }
-
-    iterator &operator++() {
-      ++index_;
-      if (index_ == sizeof(*current_)) {
-        index_ = 0;
-        ++current_;
-      }
-      return *this;
-    }
-
-    iterator operator++(int) {
-      auto tmp = *this;
-      ++(*this);
-      return tmp;
-    }
-
-    iterator &operator--() {
-      if (index_ == 0) {
-        --current_;
-        index_ = sizeof(*current_);
-      }
-      --index_;
-      return *this;
-    }
-
-    iterator operator--(int) {
-      auto tmp = *this;
-      --(*this);
-      return tmp;
-    }
-
-    iterator &operator+=(difference_type diff) {
-      // The number of bytes in one element of the underlying range.
-      const auto element_size = sizeof(*current_);
-
-      // This can be negative, so use a signed type.
-      const difference_type total_offset = index_ + diff;
-
-      // How many full elements we need to advance (or retreat).
-      const auto element_offset = total_offset / element_size;
-
-      // The new byte index within the target element.
-      // The modulo operator in C++ can yield a negative result for a negative input.
-      // We adjust it to ensure the index is always a valid, non-negative offset.
-      index_ = total_offset % element_size;
-      if (index_ < 0) {
-        index_ += element_size;
-        // We borrowed from the previous element, so adjust the element offset.
-        current_ += element_offset - 1;
-      } else {
-        current_ += element_offset;
-      }
-
-      return *this;
-    }
-
-    iterator &operator-=(difference_type diff) { return *this += -diff; }
-
-    friend iterator operator+(iterator iter, difference_type n) { return iter += n; }
-    friend iterator operator+(difference_type n, iterator iter) { return iter += n; }
-    friend iterator operator-(iterator iter, difference_type n) { return iter -= n; }
-
-    friend difference_type operator-(iterator lhs, iterator rhs) {
-      const auto elem_diff = lhs.current_ - rhs.current_;
-      const auto byte_diff = lhs.index_ - rhs.index_;
-      return elem_diff * sizeof(*current_) + byte_diff;
-    }
-
-    friend bool operator==(iterator lhs, iterator rhs) = default;
-
-    friend auto operator<=>(iterator lhs, iterator rhs) {
-      return lhs.current_ == rhs.current_ ? lhs.index_ <=> rhs.index_
-                                          : lhs.current_ <=> rhs.current_;
-    }
-  };
+  using iterator = std::conditional_t<ranges::contiguous_range<Rng>,
+                                      detail::contiguous_as_byte_iterator<BaseIter>,
+                                      detail::noncontiguous_as_byte_iterator<BaseIter>>;
 
   as_bytes_view() = default;
 
--- tests/common/range_test.cpp
+++ tests/common/range_test.cpp
@@ -3,8 +3,13 @@
 //
 
 #include "krypton/common/range.hpp"
+#include <deque>
 #include <gtest/gtest.h>
+#include <memory>
 #include <range/v3/range/concepts.hpp>
+#include <range/v3/range/conversion.hpp>
+#include <range/v3/range_fwd.hpp>
+#include <range/v3/view/concat.hpp>
 
 using namespace krypton;
 
@@ -39,5 +44,41 @@ TEST(RangeTest, AsBytesViewPipeTest) {
     expected_bytes.insert(expected_bytes.end(), byte_ptr, byte_ptr + sizeof(int));
   }
 
+  EXPECT_EQ(actual_bytes, expected_bytes);
+}
+
+TEST(RangeTest, AsBytesViewNonContiguousTest) {
+  std::deque<int> lst = {7, 14, 21};
+  auto byte_view      = lst | views::ext::as_bytes;
+
+  std::vector<uint8_t> expected_bytes;
+  for (int num : lst) {
+    uint8_t *byte_ptr = reinterpret_cast<uint8_t *>(&num);
+    expected_bytes.insert(expected_bytes.end(), byte_ptr, byte_ptr + sizeof(int));
+  }
+
+  std::vector<uint8_t> actual_bytes = byte_view | ::ranges::to_vector;
+  EXPECT_EQ(actual_bytes, expected_bytes);
+}
+
+TEST(RangeTest, AsBytesViewSizeTest) {
+  std::vector<int> vec = {1, 2, 3};
+  auto byte_view       = views::ext::as_bytes(vec);
+  EXPECT_EQ(byte_view.size(), vec.size() * sizeof(int));
+}
+
+TEST(RangeTest, AsBytesViewConcatRangeTest) {
+  std::vector<int> vec1 = {1, 2};
+  std::vector<int> vec2 = {3, 4};
+  auto concatenated     = ::ranges::views::concat(vec1, vec2);
+  auto byte_view        = views::ext::as_bytes(concatenated);
+
+  std::vector<uint8_t> expected_bytes;
+  for (int num : concatenated) {
+    uint8_t *byte_ptr = reinterpret_cast<uint8_t *>(&num);
+    expected_bytes.insert(expected_bytes.end(), byte_ptr, byte_ptr + sizeof(int));
+  }
+
+  auto actual_bytes = byte_view | ::ranges::to_vector;
   EXPECT_EQ(actual_bytes, expected_bytes);
 }
